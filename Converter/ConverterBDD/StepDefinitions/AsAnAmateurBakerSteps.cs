f57a967460ea06fd813ba0eceef8346921cac872
David Ström <david.strom88@gmail.com>
2021-05-05 14:06:48 +0200

    Lagt till step definitions

---
+++ Converter/ConverterBDD/StepDefinitions/AsAnAmateurBakerSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ConverterBDD.StepDefinitions
+{
+    [Binding]
+    public class AsAnAmateurBakerSteps
+    {
+        [Given(@"that I need an American cup")]
+        public void GivenThatINeedAnAmericanCup()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the decimals are rounded to (.*)")]
+        public void GivenTheDecimalsAreRoundedTo(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the amount of cups is (.*)")]
+        public void WhenTheAmountOfCupsIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Swedish amount is (.*) dl")]
+        public void ThenTheSwedishAmountIsDl(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
